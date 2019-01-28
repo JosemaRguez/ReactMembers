0e38b51cae1de879a2d6e6d52f91a820e8b1a3ab
JosemaCapgemini <jose-manuel.rodriguez-rabadan@capgemini.com>
2019-01-28 13:25:36 +0100

    Pagination started

--- mediasmart/frontend/src/components/members/MembersBoard.js
+++ mediasmart/frontend/src/components/members/MembersBoard.js
@@ -7,6 +7,23 @@ class MembersBoard extends Component {
         return (
             <div className='container'>
                 <MembersList />
+
+
+                <nav aria-label="Page navigation example">
+                    <ul className="pagination justify-content-center">
+                        <li className="page-item disabled">
+                            <a className="page-link" href="/" tabindex="-1">Previous</a>
+                        </li>
+                        <li className="page-item"><a className="page-link" href="/">1</a></li>
+                        <li className="page-item"><a className="page-link active" href="/">2</a> </li>
+                        <li className="page-item"><a className="page-link" href="/">3</a></li>
+                        <li className="page-item">
+                            <a className="page-link" href="/">Next</a>
+                        </li>
+                    </ul>
+                </nav>
+
+
             </div>
         )
     }
