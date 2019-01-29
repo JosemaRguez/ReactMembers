1abbbbd5ba41cf4686af322b3ad8e7a17387a0f2
Josema <rguezjosemanuel@gmail.com>
2019-01-29 22:35:43 +0100

    Ready to do cards

--- mediasmart/frontend/src/components/members/MembersList.js
+++ mediasmart/frontend/src/components/members/MembersList.js
@@ -5,31 +5,20 @@ const MembersList = (props) => {
     return (
         <div className="row">
             <div className="col s2 offset-s1 card">
-                <Link to={"/member/1"} key="1">
-                    <div>
-                        <img className="rounded-circle" src="https://pbs.twimg.com/profile_images/661718785905201152/dgLmIh3v_400x400.jpg" alt="image1" style={{width: "5em", height: "5em"}}/>
-                        <span className="card-title">Señor fardón</span>
-                    </div>
-                    <div className="card-stacked">
-                        <div className="card-content">
-                            <p>I did nothing in my whole life but I have style.</p>
+                {props.listOfMembers.map(member =>
+                    <Link to={`/member/${member.id}`} key={member.id}>
+                        <div>
+                            <img className="rounded-circle" src={member.image} alt={`${member.id} not found`} style={{ width: "5em", height: "5em" }} />
+                            <span className="card-title">{member.name}</span>
                         </div>
-                    </div>
-                </Link>
-            </div>
-
-            <div className="col s2 offset-s1 card">
-                <Link to={"/member/2"} key="2">
-                    <div>
-                        <img src="https://as01.epimg.net/img/comunes/fotos/fichas/deportistas/m/mes/large/15167.png" alt="image1" style={{width: "5em", height: "5em"}}/>
-                        <span className="card-title">Retard</span>
-                    </div>
-                    <div className="card-stacked">
-                        <div className="card-content">
-                            <p>I'm just retard.</p>
+                        <div className="card-stacked">
+                            <div className="card-content">
+                                <p>{member.age}</p>
+                            </div>
                         </div>
-                    </div>
-                </Link>
+                    </Link>
+                )
+                }
             </div>
         </div>
     )
