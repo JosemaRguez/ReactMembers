8c3d7d5f98cfe58773a0ed28f36b3fb411f8dbd4
JosemaCapgemini <jose-manuel.rodriguez-rabadan@capgemini.com>
2019-01-29 14:46:27 +0100

    Details and list done

--- mediasmart/frontend/src/components/members/MembersList.js
+++ mediasmart/frontend/src/components/members/MembersList.js
@@ -1,35 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../../styles/styles.css'
 
 const MembersList = (props) => {
+    console.log(props.listOfMembers)
     return (
-        <div className="row">
-            <div className="col s2 offset-s1 card">
-                <Link to={"/member/1"} key="1">
-                    <div>
-                        <img className="rounded-circle" src="https://pbs.twimg.com/profile_images/661718785905201152/dgLmIh3v_400x400.jpg" alt="image1" style={{width: "5em", height: "5em"}}/>
-                        <span className="card-title">Señor fardón</span>
-                    </div>
-                    <div className="card-stacked">
-                        <div className="card-content">
-                            <p>I did nothing in my whole life but I have style.</p>
+        <div className="container">
+            <div className="row">
+                {props.listOfMembers.map(member =>
+                    <Link to={`/member/${member.id}`} className="col-4 p-5" style={{ textDecoration: 'none' }} key={member.id}>
+                        <div className="card">
+                            <div className="p-1">
+                                <img className="img-fluid float-left rounded-circle p-2" src={member.image} alt={`Card ${member.id} not found`} />
+                                <p>{`${member.name}`}</p>
+                            </div>
+                            <div className="card-body">
+                                <div className="card-content">
+                                    <p className="card-text">{`Age: ${member.age}`}</p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </Link>
-            </div>
+                    </Link>
 
-            <div className="col s2 offset-s1 card">
-                <Link to={"/member/2"} key="2">
-                    <div>
-                        <img src="https://as01.epimg.net/img/comunes/fotos/fichas/deportistas/m/mes/large/15167.png" alt="image1" style={{width: "5em", height: "5em"}}/>
-                        <span className="card-title">Retard</span>
-                    </div>
-                    <div className="card-stacked">
-                        <div className="card-content">
-                            <p>I'm just retard.</p>
-                        </div>
-                    </div>
-                </Link>
+                )}
             </div>
         </div>
     )
