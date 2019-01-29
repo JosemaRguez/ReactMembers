051207d83c3b66eb0b1e19193f79d3c10c87299b
JosemaCapgemini <jose-manuel.rodriguez-rabadan@capgemini.com>
2019-01-29 16:06:35 +0100

    Loading page done

--- mediasmart/frontend/src/App.js
+++ mediasmart/frontend/src/App.js
@@ -4,15 +4,21 @@ import Navbar from './components/layouts/Navbar'
 import MembersBoard from './components/members/MembersBoard'
 import MemberDetails from './components/members/MemberDetails'
 // import MembersList from './components/members/MembersList'
+import { createBrowserHistory } from 'history'
 
 class App extends Component {
+  componentWillMount () {
+    const history = createBrowserHistory();
+    history.push('/list/1');
+  }
+
   render() {
     return (
       <BrowserRouter>
         <div className="App">
           <Navbar />
           <Switch >
-            <Route exact path='/' component={MembersBoard} />
+            <Route path='/list/:page' component={MembersBoard} />
             <Route path='/member/:id' component={MemberDetails} /> 
           </Switch>
         </div>
